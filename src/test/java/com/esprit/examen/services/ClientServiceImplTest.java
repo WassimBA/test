0bdeec6a158ca1f6b3b2290c55d6d140a4538d2d
WassimBA <73253004+WassimBA@users.noreply.github.com>
2022-10-18 11:27:24 +0100

    Update ClientServiceImplTest.java

--- src/test/java/com/esprit/examen/services/ClientServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ClientServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/*package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
 
@@ -81,4 +81,4 @@ public class ClientServiceImplTest {
 		}
 	}
 
-}
+}*/
